d33d2c06dfcd17968af20649e620b704f2586e84
unknown <jjernigan1065@gmail.com>
2019-04-09 22:31:35 -0400

    set up the basic pages and some basic components for the app. Checked to make sure everything loaded and the server ran.

---
+++ client/src/Components/signIn/signIn.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignIn = () => (
+    <div>This is the sign in Component!</div>
+)
+
+export default SignIn
---
+++ client/src/Components/wrongSignIn/WrongSignIn.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const WrongSignIn = () => (
+    <div>This is the component for when sign in fails!</div>
+)
+
+export default WrongSignIn  
